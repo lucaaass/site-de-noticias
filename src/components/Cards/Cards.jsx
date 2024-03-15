0f85c67904884b43aba4c3286e686d2f5f24239f
Lucas Marques <lucasmarques630@gmail.com>
2024-03-15 02:35:09 -0300

    finalizando o mobile

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,40 +2,34 @@ import "./Cards.css"
 const Cards = () => {
     return (
         <div className="container-cards">
-            <div>
+            <div className="cards">
                 <img src="./src/assets/image-retro-pcs.jpg" alt="" />
-                <div>
+                <div className="cards-infos">
                     <h1 className="cards-h1">01</h1>
                     <h3 className="cards-h3">Reviving Retro PCs</h3>
                     <p className="cards-p">What happens when old PCs are given modern upgrades?</p>
                 </div>
             </div>
 
-            <div>
+
+            <div className="cards">
                 <img src="./src/assets/image-top-laptops.jpg" alt="" />
-                <div>
+                <div className="cards-infos">
                     <h1 className="cards-h1">02</h1>
-                    <h3  className="cards-h3">Top 10 Laptops of 2022</h3>
+                    <h3 className="cards-h3">Top 10 Laptops of 2022</h3>
                     <p className="cards-p">Our best picks for various needs and budgets.</p>
                 </div>
             </div>
 
-            <div>
+            <div className="cards">
                 <img src="./src/assets/image-gaming-growth.jpg" alt="" />
-                <div>
+                <div className="cards-infos">
                     <h1 className="cards-h1">03</h1>
                     <h3 className="cards-h3">The Growth of Gaming</h3>
                     <p className="cards-p">How the pandemic has sparked fresh opportunities</p>
                 </div>
             </div>
-
-
-
-
-
-
         </div>
-
 
     );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,43 @@
-
+import { useState } from "react";
 
 import './Header.css'
-const Header = () => {
-    return ( 
-        <div className='container-header'>
-            <div className='logo'>
-                <li>W.</li>
-            </div>
 
-            <div className='list'>
-                    <li>Home</li>
-                    <li>New</li>
-                    <li>Popular</li>
-                    <li>Treding</li>
-                    <li>Categories</li>
+function Header() {
+    const [active, setMode] = useState(false);
+    const ToggleMode = () => {
+        setMode(!active)
+    }
+
+
+    return (
+
+        <div className="App">
+            <div>
+                <ul className='logo'>
+                    <li>W.</li>
+                </ul>
             </div>
+            <div className={active ? 'icon iconActive' : 'icon'} onClick={ToggleMode}>
+                <div className="hamburguer"><img src="./src/assets/icon-menu.svg" alt="" /></div>
+                <div className="hamburguerClose hambuerguerIcon"><img src="./src/assets/icon-menu-close.svg" alt="" /></div>
             </div>
-     );
+            <div className={active ? 'menu menoOpen' : 'menu menuClose'}>
+                <div className="list">
+
+                    <ul className="listItems" >
+                        <li >Home</li>
+                        <li >New</li>
+                        <li >Popular</li>
+                        <li >Trending</li>
+                        <li >Categories</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+
+
+    )
 }
- 
+
 export default Header;
